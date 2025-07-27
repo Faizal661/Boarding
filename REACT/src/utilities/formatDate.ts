1abab1795729a1be227949f8654bfdd520887a2b
faizal <faisalt661@gmail.com>
2025-07-27 16:05:46 +0530

    wip : comment section in react

---
+++ REACT/src/utilities/formatDate.ts
@@ -0,0 +1,9 @@
+export const formatDate = (date: Date) => {
+  return date.toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "short",
+    year: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+};
