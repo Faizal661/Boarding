0282f361e293a6976badb8fe54bc11ba37e90bbf
faizal <faisalt661@gmail.com>
2025-08-09 21:37:10 +0530

    dsa workouts

--- JS/flat_method.js
+++ JS/flat_method.js
@@ -1,6 +1,27 @@
-let arr=[1,2,3,4,5,[1,2,3,4,5,[1,2,3,4,5,[2,3,3,4],6,7,8,],7,6,5,]]
+const arr = [1, 2, 3, null, [4, 5, 6, [7, 8, 9, 10], 11, 12, 13, 14, [15, [[16, 17]]]]]
 
 // let flattedArr=arr.flat(1)
-let flattedArr=arr.flat(Infinity)
+// let flattedArr=arr.flat(Infinity)
 
-console.log(flattedArr);
+// console.log(flattedArr);
+
+
+//============ Manual alternaive for flat =======
+
+function flatThisArray(arr, depth = 1) {
+    let res = []
+
+    for (const item of arr) {
+        if (Array.isArray(item) && depth > 0) {
+            let nested = flatThisArray(item, depth - 1)
+            res.push(...nested)
+        } else {
+            res.push(item)
+        }
+
+    }
+
+    return res
+}
+
+console.log(flatThisArray(arr, 2))
